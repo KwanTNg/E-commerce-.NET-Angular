74ce95422a213b84d56c01087a2d1d322c90d103
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-23 08:20:10 +0100

    Section 2.7 Reviewing the Projects in VS Code

--- API/Program.cs
+++ API/Program.cs
@@ -3,20 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+// Anything before this line is considered service
+// Anything after this line is considered middleware
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
