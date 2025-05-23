46d6a5638a72975ab3eb1b9b15fe2f8053e02e70
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-23 14:37:43 +0100

    Section 2.15 Adding the update and delete endpoints

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a46d499cbd852d5e594c908214991f5a1164037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f1de174a4fadf1ba5f634fb46859c0ead6d5a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
