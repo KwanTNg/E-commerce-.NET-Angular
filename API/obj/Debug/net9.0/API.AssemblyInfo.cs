e46c7c5e4f4ac8929730c80fe9c9d3476e2fd473
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-25 10:11:28 +0100

    Section 5.53 Validation Error Responses (Create DTOs for error handling)

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a6cf292a957d5c3d049859d5648060b7ebb384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2338ac098efea556199de02a9fae026da52dd346")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
