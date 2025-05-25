ebd55bd10fbd5c2b4ae79ebcd52df4433e44d361
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-25 19:34:39 +0100

    Section 8.67 Making http requests in Angular

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a640f6ff672745bcff8bbb0a9ab0d2f17fd55b18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b27cb783a580140ccbd81087bf9f9e8c48008bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
