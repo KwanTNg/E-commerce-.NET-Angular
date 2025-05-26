e8caebe294411469e4ff1e5f5aedc9e4a310f9fc
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-26 09:51:02 +0100

    Section 9.77 Getting the types and brands lists

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -9,8 +9,31 @@ import { Product } from '../../shared/models/product';
 export class ShopService {
   baseUrl = 'https://localhost:5203/api/';
   private http = inject(HttpClient);
+  //Service is a singleton, properties stored available lifetime
+  //types and brands are static lists, should store here
+  //shop component eventually disposed when routing
+  //avod keep requesting from API
+  types: string[] = []
+  brands: string[] = []
   
+  //products store in component, should subscribe there
   getProducts() {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products?pageSize=20')
+  }
+
+  //since types and brands are stored here, should subscribe here
+  getBrands() {
+    //to prevent it keep executing
+    if (this.brands.length > 0) return;
+    return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
+      next: response => this.brands = response
+    })
+  }
+
+  getTypes() {
+    if (this.types.length > 0) return;
+    return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
+      next: response => this.brands = response
+    })
   }
 }
