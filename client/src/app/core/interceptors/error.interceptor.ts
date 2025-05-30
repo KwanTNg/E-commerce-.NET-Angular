0f6059374383d408ab101dd72566100f22f7cc69
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-30 11:58:47 +0100

    Section 11.96 Adding toast (snackbar) notifications

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -2,18 +2,21 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
+import { SnackbarService } from '../services/snackbar.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   //If we want to do something on the way out, we have to do it before the request
   //If way back, after the request
   const router = inject(Router);
+  const snackbar = inject(SnackbarService);
+
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
       if (err.status === 400) {
-        alert(err.error.title || err.error)
+        snackbar.error(err.error.title || err.error)
       }
       if (err.status === 401) {
-        alert(err.error.title || err.error)
+        snackbar.error(err.error.title || err.error)
       }
       if (err.status === 404) {
         router.navigateByUrl('/not-found');
