2df77adf53ae650bb7159514254d79f0448a03d6
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-31 18:19:32 +0100

    Section 14.131 Updating the DB and testing the endpoints

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99e722cd3b4863f3653444b4fd68c7d120526b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec2190dc127527abda62e74c41b14452649f8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
