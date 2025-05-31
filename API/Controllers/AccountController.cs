af6f51c313037d2e4c2e09b59d01133e6adc6df6
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-05-31 19:35:19 +0100

    Section 14.136 Validation errors

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -21,7 +21,15 @@ public class AccountController(SignInManager<AppUser> signInManager) : BaseApiCo
         };
 
         var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
-        if (!result.Succeeded) return BadRequest(result.Errors);
+        // Change the format of validation error return so taht we can handle the error easily
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+            return ValidationProblem();
+        }
         return Ok();
     }
 
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0e0998f37c3a1a7c6d491d1efe6c179b38817d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71b0b0340ceee5d0edae8179e11c6282732a146")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
