be87378b8f17e62ca8814013544525b26f3fef26
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-02 17:54:47 +0100

    Section 17.184 Creating the order aggregate part 1

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca4a99e0938c015b4caf963bcae9637c0a7a7bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01bd69049b3bcf7c03ba5e653c77be31e79c8d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
