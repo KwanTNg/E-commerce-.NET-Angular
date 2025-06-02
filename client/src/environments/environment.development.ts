603ca12270d30c68d0c45ee953acc9362d668c2f
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-02 08:32:09 +0100

    Section 16.165 Adding client side Stripe

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5203/api/'
+    apiUrl: 'https://localhost:5203/api/',
+    stripePublicKey: 'pk_test_51RVG6cPOT4AklRnjHcN3mNRaL4eRj0hsYjvSYmHvWKcJWH2Zh4H32Ob8MPyoYAlEzRw5SiVHbBwXd9cdpYXiKs9v00S75aWFS9'
 };
