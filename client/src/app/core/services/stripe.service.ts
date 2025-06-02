711ac16cf3f35ba28327a77362c3fcaf57146a0c
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-02 08:52:33 +0100

    Section 16.166 Creating the address element

--- client/src/app/core/services/stripe.service.ts
+++ client/src/app/core/services/stripe.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { loadStripe, Stripe, StripeElement, StripeElements } from '@stripe/stripe-js';
+import { loadStripe, Stripe, StripeAddressElement, StripeAddressElementOptions, StripeElement, StripeElements } from '@stripe/stripe-js';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CartService } from './cart.service';
@@ -15,6 +15,7 @@ export class StripeService {
   private http = inject(HttpClient);
   private stripePromise : Promise<Stripe | null>;
   private elements?: StripeElements;
+  private addressElements?: StripeAddressElement;
 
   constructor() {
     this.stripePromise = loadStripe(environment.stripePublicKey);
@@ -24,6 +25,8 @@ export class StripeService {
     return this.stripePromise;
   }
 
+  //we only want only single instance of stripe
+  //becasue stripe return promise, so we use async
   async intializeElements() {
     if (!this.elements) {
       const stripe = await this.getStripeInstance();
@@ -36,6 +39,21 @@ export class StripeService {
       }
     }
     return this.elements;
+  }
+
+  async createAddressElement() {
+    if (!this.addressElements) {
+      const elements = await this.intializeElements();
+        if (elements) {
+          const options: StripeAddressElementOptions = {
+            mode: 'shipping'
+          };
+          this.addressElements = elements.create('address', options);
+        } else {
+          throw new Error('Elements instance has not been loaded');
+        }
+    }
+    return this.addressElements;
   }
 
   createOrUpdatePaymentIntent(){
