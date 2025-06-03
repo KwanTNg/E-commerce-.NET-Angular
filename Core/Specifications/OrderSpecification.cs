5c58d92600e9bbb1f75ec36b17bd94d761d78c85
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-03 18:05:24 +0100

    Section 18.203 Adding a webhook endpoint in the payments controller

--- Core/Specifications/OrderSpecification.cs
+++ Core/Specifications/OrderSpecification.cs
@@ -21,4 +21,11 @@ public class OrderSpecification : BaseSpecification<Order>
         //we can also use AddInclude(x => x.OrderItems); AddInclude(x => x.DeliveryMethod);
     }
 
+    public OrderSpecification(string paymentIntentId, bool isPaymentIntent) :
+        base(x => x.PaymentIntentId == paymentIntentId)
+    {
+        AddInclude("OrderItems");
+        AddInclude("DeliveryMethod");   
+    }
+
 }
