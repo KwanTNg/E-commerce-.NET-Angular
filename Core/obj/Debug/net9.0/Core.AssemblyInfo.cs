c353d3402131e96a26d8193880cd2230cc271dd5
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-03 13:59:06 +0100

    Section 17.195 Shaping the data to return

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ce572f5d72805e360d43d89a3ec12a3a42b390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e71b6d14496e5ad731489f3869683fcdb8b98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
