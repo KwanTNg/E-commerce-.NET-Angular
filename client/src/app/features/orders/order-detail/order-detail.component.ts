07ef628d6e1a60157367ce22121775e6ac5b3867
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-03 16:42:48 +0100

    Section 18.201 Creating the order detailed page

--- client/src/app/features/orders/order-detail/order-detail.component.ts
+++ client/src/app/features/orders/order-detail/order-detail.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../core/services/order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../../../shared/models/order';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-detail',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButton,
+    DatePipe,
+    CurrencyPipe
+  ],
   templateUrl: './order-detail.component.html',
   styleUrl: './order-detail.component.scss'
 })
-export class OrderDetailComponent {
+export class OrderDetailComponent implements OnInit {
+  private orderService = inject(OrderService);
+  private activatedRoute = inject(ActivatedRoute);
+  order?: Order;
+
+  ngOnInit(): void {
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if(!id) return;
+    this.orderService.getOrderDetailed(+id).subscribe({
+      next: order => this.order = order
+    })
+  }
 
 }
