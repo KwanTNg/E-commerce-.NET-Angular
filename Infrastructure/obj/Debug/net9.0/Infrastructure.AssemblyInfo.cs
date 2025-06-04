0da899cd2013bc644351c6cb8bf39754dec101bf
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-04 12:16:49 +0100

    Section 19.214 Setting up a cloud Redis server

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e47c86c36484c7bf03a53a0f4bdb9b4be006d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0a5fb1a35ca3d7b6a4c561f9121c4c25027bcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
