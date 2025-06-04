0062ee156e5ea4bc72e39692af2f142a51fa84b4
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-04 09:27:41 +0100

    Section 18.207 Updating the checkout success page

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfd999c5a4c11c66ccf1bb079339c45494268bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ed26a08e6f1c55c279a537884460f78187ebd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfd999c5a4c11c66ccf1bb079339c45494268bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ed26a08e6f1c55c279a537884460f78187ebd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
