4f24ec4fff15fccff80139db488a6a7d7d3c9d67
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-05 17:07:53 +0100

    Section 21.239 Adding a confirmation prompt

--- client/src/app/features/admin/admin.component.ts
+++ client/src/app/features/admin/admin.component.ts
@@ -10,6 +10,7 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterLink } from '@angular/router';
+import { DialogService } from '../../core/services/dialog.service';
 
 @Component({
   selector: 'app-admin',
@@ -32,6 +33,7 @@ export class AdminComponent implements OnInit {
   displayedColumns: string[] = ['id', 'buyerEmail', 'orderDate', 'total', 'status', 'action'];
   dataSource = new MatTableDataSource<Order>([]);
   private adminService = inject(AdminService);
+  private dialogService = inject(DialogService);
   orderParams = new OrderParams();
   totalItems = 0;
   statusOptions = ['All', 'PaymentReceived', 'PaymentMismatch', 'Refunded', 'Pending']
@@ -66,6 +68,15 @@ export class AdminComponent implements OnInit {
       this.loadOrders();
     }
   
+  async openConfirmDialog(id: number) {
+    const confirmed = await this.dialogService.confirm(
+      'Confirm refund',
+      'Are you sure you want to issue this refund? This cannot be undone'
+    )
+
+    if (confirmed) this.refundOrder(id);
+  }
+
   refundOrder(id: number) {
     this.adminService.refundOrder(id).subscribe({
       next: order => {
