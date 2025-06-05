c880021df4e39fd919ed5b1a2b24533b6f0337fa
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-06-05 14:29:12 +0100

    Section 21.233 Creating an admin route guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { OrderComponent } from './features/orders/order/order.component';
 import { OrderDetailComponent } from './features/orders/order-detail/order-detail.component';
 import { orderCompleteGuard } from './core/guards/order-complete.guard';
 import { AdminComponent } from './features/admin/admin.component';
+import { adminGuard } from './core/guards/admin.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -31,6 +32,6 @@ export const routes: Routes = [
     {path: 'test-error', component: TestErrorComponent},
     {path: 'not-found', component: NotFoundComponent},
     {path: 'server-error', component: ServerErrorComponent},
-    {path: 'admin', component: AdminComponent, canActivate: [authGuard]},
+    {path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard]},
     {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
